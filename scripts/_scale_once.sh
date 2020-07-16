0cd83407f7c83f167715bc3c92caff968d73df26
Lewis Daly <lewisdaly@me.com>
2020-07-16 21:50:50 +0800

    more helm work

--- scripts/_scale_once.sh
+++ scripts/_scale_once.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HELM_ONCE_DIR="${DIR}/../helm-once"
 ENV_FILE=${ENV_FILE:=${DIR}/../.env}
+DRY_RUN_FLAG=${DRY_RUN_FLAG:="--dry-run"}
 set -a; source ${ENV_FILE} ;set +a
 
 echo "${PATH_TO_CONFIG}"
@@ -12,8 +13,8 @@ set -e
 
 helm install \
   --set-file config.config_js="${PATH_TO_CONFIG}" \
-  --set secrets.CATTLE_SECRET_KEY=${CATTLE_SECRET_KEY} \
-  --set secrets.RANCHER_BASE_URL=${RANCHER_BASE_URL} \
-  --set secrets.SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL} \
+  --set secret.CATTLE_SECRET_KEY=${CATTLE_SECRET_KEY} \
+  --set secret.RANCHER_BASE_URL=${RANCHER_BASE_URL} \
+  --set secret.SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL} \
   --set job.npmCommand=${1} \
-  rancher-scaler-once ${HELM_ONCE_DIR}
+  rancher-scaler-once ${HELM_ONCE_DIR} ${DRY_RUN_FLAG}
