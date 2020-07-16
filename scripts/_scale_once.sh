f185ba8ae2caa44bef799493f45f9d2a1d8a5aa9
Lewis Daly <lewisdaly@me.com>
2020-07-16 13:53:20 +0800

    update npm run scripts

--- scripts/_scale_once.sh
+++ scripts/_scale_once.sh
@@ -5,12 +5,15 @@ HELM_ONCE_DIR="${DIR}/../helm-once"
 ENV_FILE=${ENV_FILE:=${DIR}/../.env}
 set -a; source ${ENV_FILE} ;set +a
 
+echo "${PATH_TO_CONFIG}"
+
 set -u
 set -e
 
 helm install \
+  --set-file config.config_js="${PATH_TO_CONFIG}" \
   --set secrets.CATTLE_SECRET_KEY=${CATTLE_SECRET_KEY} \
   --set secrets.RANCHER_BASE_URL=${RANCHER_BASE_URL} \
   --set secrets.SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL} \
   --set job.npmCommand=${1} \
-  rancher-scaler-once ${HELM_ONCE_DIR} --dry-run
+  rancher-scaler-once ${HELM_ONCE_DIR}
